87cf7ce5f8e13f7b135bc3eb1b0f90db81f619d8
Alessandro Cristofori <alessandro.cristofori@thinkwhere.com>
2016-03-11 16:56:56 +0000

    3d with WFS layer

--- js/3dmap.js
+++ js/3dmap.js
@@ -13,7 +13,29 @@ var layer = new ol.layer.Tile({
   })
 });
 
-var WFSLayer = new ol.layer.Tile({});
+var vectorSource = new ol.source.Vector({
+  loader: function(extent, resolution, projection) {
+    var url = "http://pkc.locationcentre.co.uk/data_services/wfs.ashx?id=w8cuE73dsKQRQCDfO-YBhw~~&SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=PKC:Solar_Panels_Standard_Applications_545&SRSNAME=EPSG:4326&BBOX=" + extent.join(",");
+    $.ajax({url: url, dataType: 'jsonp', jsonp: false});
+  },
+  strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({
+    maxZoom: 19
+  }))
+});
+
+window.loadFeatures = function(response) {
+  vectorSource.addFeatures(geojsonFormat.readFeatures(response));
+};
+
+var vector = new ol.layer.Vector({
+  source: vectorSource,
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'rgba(0, 0, 255, 1.0)',
+      width: 2
+    })
+  })
+});
 
 var ol2d = new ol.Map({
   layers: [layer],
