a5f108de19667008a0db222adfe549ec3dc10e46
Alessandro Cristofori <alessandro.cristofori@thinkwhere.com>
2016-03-11 15:34:14 +0000

    OS 2d map working

--- js/3dmap.js
+++ js/3dmap.js
@@ -1,14 +1,22 @@
+var projection = new ol.proj.Projection({
+                code: 'EPSG:27700',
+                extent: [0.0, 0.0, 700000.0, 1300000.0],
+                units: 'm'
+            });
+            ol.proj.addProjection(projection);
+
 var view = new ol.View({
-  projection: 'EPSG:27700',
-  center: ol.proj.transform([-4.71382, 57.16617], 'EPSG:4326', 'EPSG:3857'),
+  projection: projection,
+  center: [278202, 694687],
   zoom: 7
 });
 
 var layer = new ol.layer.Tile({
   source: new ol.source.TileWMS({
-    url: 'http://wms.locationcentre.co.uk/service/thinkwhere1234/service?',
+    url: 'http://wms.locationcentre.co.uk/services/thinkwhere1234/service?',
     params: {
-      'LAYERS': 'os_background_bng_colour'
+      'LAYERS': 'os_background_bng_colour',
+      'FORMAT' : 'image/png'
     }
   })
 });
@@ -27,4 +35,4 @@ var terrainProvider = new Cesium.CesiumTerrainProvider({
 });
 scene.terrainProvider = terrainProvider;
 
-ol3d.setEnabled(true);
+ol3d.setEnabled(false);
